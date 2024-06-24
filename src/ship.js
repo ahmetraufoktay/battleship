8f2dd4f010adfe02b75a7d57e6703dff2056e406
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-06-24 14:30:47 +0300

    [FEAT-1] Add ship.js Add functions to ship.js according to the tests of ship.test.js

---
+++ src/ship.js
@@ -0,0 +1,17 @@
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.timesHit = 0;
+    this.sunk = false;
+  }
+  hit() {
+    this.timesHit =
+      this.length > this.timesHit ? this.timesHit + 1 : this.length;
+  }
+  isSunk() {
+    this.sunk = this.timesHit === this.length ? true : false;
+    return this.sunk;
+  }
+}
+
+export { Ship };
