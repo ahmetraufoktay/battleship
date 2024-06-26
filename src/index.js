1f745d4d2b2de3bbec0639a93521dcdb802f1acc
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-06-26 15:13:24 +0300

    [FEAT-9] Add square function Create a square function to load squares into players in the beginning of the game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./style.css";
+import { Gameboard } from "./gameboard";
+
+const playerOne = document.getElementById("playerOne");
+const playerTwo = document.getElementById("playerTwo");
+
+function createSquare(x, y) {
+  const block = document.createElement("div");
+  block.classList = "tile";
+  block.setAttribute("data-x", x);
+  block.setAttribute("data-y", y);
+  return block;
+}
+
+function loadSquares(player) {
+  for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+      const myblock = createSquare(i, j);
+      player.appendChild(myblock);
+    }
+  }
+}
+
+loadSquares(playerOne);
+loadSquares(playerTwo);
