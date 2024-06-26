3d2c83546052679099216613baa86869cf46fc26
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-06-26 23:38:14 +0300

    [REFAC-5] Add name menu Add name menu to the start of the game

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import "./style.css";
 import { Player } from "./player";
-import { twoPlayerMain } from "./table";
+import { tableMain } from "./table";
+import { playerOneDiv } from "./selection";
+import { nameMenu } from "./start";
 
 const body = document.querySelector("body");
 
 const playerOne = new Player("human");
 const playerTwo = new Player("human");
 
-body.appendChild(twoPlayerMain);
+body.appendChild(nameMenu);
+const startButton = document.getElementById("nameButton");
 
 export { playerOne, playerTwo };
