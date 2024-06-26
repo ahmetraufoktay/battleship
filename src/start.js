f8c12a49d8e3f29b8164068bcf4d3c2789f7bf49
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-06-26 23:32:11 +0300

    [FEAT-11] Create start menu Create a start menu file for the game

---
+++ src/start.js
@@ -0,0 +1,22 @@
+const label = document.createElement("div");
+label.innerText = "Enter Your Name Captain";
+label.id = "label";
+
+const input = document.createElement("input");
+input.id = "nameInput";
+input.placeholder = "YOUR NAME";
+
+const button = document.createElement("button");
+button.innerText = "CONFIRM";
+button.id = "nameButton";
+
+const nameArea = document.createElement("div");
+nameArea.id = "nameArea";
+nameArea.appendChild(label);
+nameArea.appendChild(input);
+nameArea.appendChild(button);
+
+const nameMenu = document.createElement("main");
+nameMenu.appendChild(nameArea);
+
+export { nameMenu };
